5ec9043e0d0da25a9c165a8be03fe97032f8cf3e
darango3002 <donaldarango@ufl.edu>
2023-02-07 21:07:08 -0500

    added: fixed value of StringLitToken

--- src/edu/ufl/cise/plcsp23/StringLitToken.java
+++ src/edu/ufl/cise/plcsp23/StringLitToken.java
@@ -16,8 +16,37 @@ public class StringLitToken extends Token implements IStringLitToken{
     public String getValue() {
         String val = "";
         for (int i = 0; i < length; i++) {
-            val += source[pos + i];
+            char ch = source[pos + i];
+            if (ch == '\\') {
+                char nextChar = getEscapeSequence(source[pos + i + 1]);
+                val += nextChar;
+                i++;
+            }
+            else {
+                val += ch;
+            }
         }
         return val.substring(1, val.length() - 1);
+    }
+
+    private char getEscapeSequence(char ch) {
+        if (ch == 'b') {
+            return '\b';
+        }
+        else if (ch == 't') {
+            return '\t';
+        }
+        else if (ch == 'n') {
+            return '\n';
+        }
+        else if (ch == 'r') {
+            return '\r';
+        }
+        else if (ch == '"') {
+            return '\"';
+        }
+        else {
+            return ch;
+        }
     }
 }
