48986e03ea0abae1dee669c3a43e11b342fce498
darango3002 <donaldarango@ufl.edu>
2023-02-20 23:48:26 -0500

    added assignment 2 files

---
+++ src/edu/ufl/cise/plcsp23/Parser.java
@@ -0,0 +1,14 @@
+package edu.ufl.cise.plcsp23;
+
+import edu.ufl.cise.plcsp23.ast.AST;
+
+public class Parser implements IParser{
+
+    Parser(String input) {
+
+    }
+    @Override
+    public AST parse() throws PLCException {
+        return null;
+    }
+}
